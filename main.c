1b9fe7e4276bfd761b421354473dfe09b83ab0ff
andersonms1 <andersoonn.ams@gmail.com>
2017-08-12 11:07:17 -0300

    adding exercices

--- main.c
+++ main.c
@@ -2,8 +2,43 @@
 #include <stdlib.h>
 
 
+/*
+Lucas Gomes de Oliveira
+entradas: dois numeros inteiros
+saidas: a soma dos numedos
+*/
+
+/*
+Wellingthon Diego Resende de Lima
+entradas: dois numeros inteiros
+saidas: subtração do primeiro pelo segundo
+*/
+
+/*
+João Vitor Ramos de Souza
+entradas: dois numeros inteiros
+saidas: o maior dos numeros
+*/
+
+/*
+Victor Alves Gomide
+entradas: dois numeros inteiros
+saidas: o menor dos numeros
+*/
+
+/*
+Bruno Oliveira Dantas
+entradas: um numero inteiro
+saidas: quadrado da entrada
+*/
+
+/*
+Martha Dantas Silva
+entradas: um numero inteiros
+saidas: cubo da entrada
+*/
 
 int main(){
-    printf("Hello darkness, my old friend\n");
+
     return 0;
 }
